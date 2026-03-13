30794fdd54a146d0053e15ab6af639cad7704d4d
Ejaz47-Hub <ejaz47026@gmail.com>
2026-03-13 08:08:38 +0530

    Development

--- src/app.js
+++ src/app.js
@@ -25,4 +25,15 @@ app.post("/create-post", upload.single("image"),async(req, res)=>{
     })
 })
 
-export default app;
+app.get("/posts",async(req,res)=>{
+    const posts = await postModel.find()
+    
+    res.status(200).json({message:"posts Fetched",
+        posts
+    })
+    
+    
+
+})
+
+export default app; 
